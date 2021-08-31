eb76a8701891dee30d2687cf27fa39f9f56efb3b
godban <89866376+godban@users.noreply.github.com>
2021-08-31 21:44:25 +0300

    Add files via upload

---
+++ precache-manifest.244b759362bc689c39bd2eb07909e511.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125071884126ab937fe41f26b7593fbd",
+    "url": "/browsers-support-badges/index.html"
+  },
+  {
+    "revision": "5a327999544971ad5916",
+    "url": "/browsers-support-badges/static/css/2.02d6bbd9.chunk.css"
+  },
+  {
+    "revision": "2a9e0835154d38bc0581",
+    "url": "/browsers-support-badges/static/css/main.76fe0199.chunk.css"
+  },
+  {
+    "revision": "5a327999544971ad5916",
+    "url": "/browsers-support-badges/static/js/2.d275db3c.chunk.js"
+  },
+  {
+    "revision": "0a08d1e98b946f2f20ef33c46104a0a6",
+    "url": "/browsers-support-badges/static/js/2.d275db3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a9e0835154d38bc0581",
+    "url": "/browsers-support-badges/static/js/main.4eb20cee.chunk.js"
+  },
+  {
+    "revision": "7718c8ec15669f798a09",
+    "url": "/browsers-support-badges/static/js/runtime-main.1a9ef2cd.js"
+  },
+  {
+    "revision": "54693c71220367de907552710b4dc2bd",
+    "url": "/browsers-support-badges/static/media/book.54693c71.svg"
+  },
+  {
+    "revision": "1899110c1bc0f5f0037039003e166720",
+    "url": "/browsers-support-badges/static/media/check.1899110c.svg"
+  },
+  {
+    "revision": "a78a81f8d29d860f8d4bf7949d02b69b",
+    "url": "/browsers-support-badges/static/media/chevron-down.a78a81f8.svg"
+  },
+  {
+    "revision": "35a09128707e840662743c4f4e25d898",
+    "url": "/browsers-support-badges/static/media/chevron-up.35a09128.svg"
+  },
+  {
+    "revision": "60cabd4b202d46fe182b7ccee0f609a6",
+    "url": "/browsers-support-badges/static/media/clippy.60cabd4b.svg"
+  },
+  {
+    "revision": "c66e419564e8ae3db63950a574ba6a4f",
+    "url": "/browsers-support-badges/static/media/mark-github.c66e4195.svg"
+  },
+  {
+    "revision": "a0f8f5984e19d467ac8207a1e9fa26fb",
+    "url": "/browsers-support-badges/static/media/x.a0f8f598.svg"
+  }
+]);
